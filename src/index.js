7399301272837ea39465334bfc692af118f9118f
Agon Karimani <agonkarimani@gmail.com>
2021-04-27 12:32:52 +0200

    Add Exercise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import AppHeader from './components/AppHeader'
+console.clear()
 
-document.body.append(AppHeader('JavaScript App Template'))
+const box = document.querySelector('.box')
+const button = document.querySelector('.button')
+button.addEventListener('click', () => {
+  box.classList.add('blue')
+})
+
+const remove = document.querySelector('.remove')
+remove.addEventListener('click', () => {
+  box.classList.remove('blue')
+})
+
+const toggle = document.querySelector('.toggle')
+toggle.addEventListener('click', () => {
+  box.classList.toggle('blue')
+})
